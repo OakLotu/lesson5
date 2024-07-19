cc907ed7e9286e82080b9c5d2148391a0f36d4ea
OakLotu <danyakalinin1201@gmail.com>
2024-07-19 11:18:24 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -1,3 +1,6 @@
 immutable_var = 1, 2, 'a', 'b'
 print (immutable_var)
-immutable_var[0] = 5
+#невозможно изменить из-за того что Объект «кортеж» не поддерживает назначение элементов
+mutable_list = [1, 2, 'ab', 'ba']
+mutable_list[0] = 3
+print (mutable_list)
